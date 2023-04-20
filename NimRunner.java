2ab83aa31542d34ab11e11421c69bf1b9cfef253
hdwyerr <hedwyer20@gmail.com>
2023-04-20 13:47:20 -0400

    added header of minimax method

---
+++ NimRunner.java
@@ -0,0 +1,24 @@
+public class NimRunner{
+    public static void main(String[] args){
+
+    }
+    //run game returns boolean
+    //get x and y move return ints
+    //some kind of dsiplay or printing method (state)
+
+    public static boolean runGame(){
+        
+    }
+
+    public static int getXMove(/*state*/){
+
+    }
+
+    public static int getYMove()/*state*/{
+
+    }
+
+    public static int minimax(){
+        
+    }
+}
