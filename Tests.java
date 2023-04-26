654a6ff3eb797424908572fea69b33f83e33c029
hdwyerr <hedwyer20@gmail.com>
2023-04-26 18:13:04 -0400

    added some test cases

--- Tests.java
+++ Tests.java
@@ -1,7 +1,12 @@
 public class Tests{
     public static void main(String[] args){
         System.out.println("Testing NimRunner class:");
-        System.out.println(NimRunner.minimax(1, true) == -1);
-        System.out.println(NimRunner.minimax(0,true) == 1);
+        System.out.println(NimRunner.minimax(1, true) == -1);//should be true
+        System.out.println(NimRunner.minimax(0,true) == 1);//should be true
+        System.out.println(NimRunner.minimax(1,false) == 1);//should be true;
+        System.out.println(NimRunner.minimax(2, true) == 1); //should be true;
+        System.out.println(NimRunner.minimax(2, false) == -1);;// ture; 
+        System.out.println(NimRunner.minimax(3, true) ==1);
+        System.out.println(NimRunner.minimax(3, false) == -1);
     }
 }
