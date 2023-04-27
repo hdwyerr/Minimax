2a61db7e59368a2f480a39cb260c7e92e0e7e55d
hdwyerr <hedwyer20@gmail.com>
2023-04-26 22:09:53 -0400

    worked on testing bestMove

--- Tests.java
+++ Tests.java
@@ -8,5 +8,19 @@ public class Tests{
         System.out.println(NimRunner.minimax(2, false) == -1);;// ture; 
         System.out.println(NimRunner.minimax(3, true) ==1);
         System.out.println(NimRunner.minimax(3, false) == -1);
+        System.out.println(NimRunner.minimax(4, true) == 1);
+        System.out.println(NimRunner.minimax(4, false) ==-1);
+        System.out.println(NimRunner.minimax(6, true) ==1);
+        System.out.println(NimRunner.minimax(6, false) == -1);
+        System.out.println(NimRunner.minimax(5, true) == -1);
+        System.out.println(NimRunner.minimax(5, false) == 1);
+        System.out.println("BEST MOVE");
+
+        System.out.println(NimRunner.bestMove(2, true) == 1);
+        System.out.println(NimRunner.bestMove(2, false) == 1);
+        System.out.println(NimRunner.bestMove(3, true) == 2);
+        System.out.println(NimRunner.bestMove(4, true) == 3);
+        System.out.println(NimRunner.bestMove(6, true));
+        System.out.println(NimRunner.bestMove(7, true));
     }
 }
